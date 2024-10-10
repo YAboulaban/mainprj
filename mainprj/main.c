12d8b85b5ff840f4e2e22ef8feea7e2e5ed7565f
Yosif Aboulaban <aboulabany@gmail.com>
2024-10-10 06:45:11 +0300

    Initial Commit

---
+++ mainprj/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  mainprj
+//
+//  Created by Yosif Aboulaban on 10.10.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
